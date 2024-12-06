76629500cd85d5ba96b3974bf6799dc61e705bf0
moligen <76134384+moligen@users.noreply.github.com>
2024-12-06 14:24:02 +0300

    app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def run_telegram_bot():
 
 if __name__ == '__main__':
     db.create_all()  # Veritabanını oluştur
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=4000)
